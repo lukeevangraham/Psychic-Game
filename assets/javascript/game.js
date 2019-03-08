f8657108c94c2b53ac2184b5eb643df70681fd97
Cefalu Wave <luke.evan.graham@gmail.com>
2019-03-08 10:28:55 -0800

    working on the beginning

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,25 @@
-var hello = 0;
+// APP RANDOMLY PICKS A LETTER
+    // var chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    // var stringLength = 1;
+    // var randomLetter = '';
+    // for (let i = 0; i < stringLength; i++) {
+    //     // const element = array[i];
+    //     var rnum = Math.floor(Math.random() * chars.length);
+    //     randomLetter += chars.substring(rnum,rnum+1);
+    // }
+    // document.randform.randomfield.value = randomLetter;
 
-console.log(hello);
+// Pasted from web as template
+    function randomString() {
+        var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+        var string_length = 8;
+        var randomstring = '';
+        for (var i=0; i<string_length; i++) {
+            var rnum = Math.floor(Math.random() * chars.length);
+            randomstring += chars.substring(rnum,rnum+1);
+        }
+        document.randform.randomfield.value = randomstring;
+        console.log(randomstring);
+    }
+
+// USER GUESSES WHICH LETTER THE APP CHOSE
