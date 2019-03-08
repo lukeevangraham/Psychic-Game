c1e5cb31651e411932f6a91466b6e05bad5b5f45
Cefalu Wave <luke.evan.graham@gmail.com>
2019-03-08 11:48:47 -0800

    got the game to display YOU LOSE after 9 guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,18 +9,41 @@ var losses = 0;
 var guessesLeft = 0;
 var guesses = [];
 
-if (guesses.length <= 8) {
+// if (guesses.length <= 8) {
+//     // USER GUESSES WHICH LETTER THE APP CHOSE
+
+//     // This function runs whenever the user presses a key.
+//     document.onkeyup = function(event) {
+
+//         // Determines which key was pressed.
+//         var userGuess = event.key;
+//         guesses.push(userGuess);
+//         console.log(guesses);
+//         console.log(guesses.length);
+//     }
+// } else {
+//     console.log("YOU LOSE!")
+// }
+
+// for (let i = 0; i < 9; i++) {
     // USER GUESSES WHICH LETTER THE APP CHOSE
 
     // This function runs whenever the user presses a key.
     document.onkeyup = function(event) {
 
-        // Determines which key was pressed.
-        var userGuess = event.key;
+        if (guesses.length < 9) {
+
+            var userGuess = event.key;
         guesses.push(userGuess);
         console.log(guesses);
         console.log(guesses.length);
-    }
-} else {
-    console.log("YOU LOSE!")
+            
+        } else {
+            console.log("YOU LOSE");
+        }
+
+        // Determines which key was pressed.
+        
+    // }
+    
 }
