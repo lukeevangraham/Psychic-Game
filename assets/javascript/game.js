0610fae0e77e518aefe2dde5590f121a58b39477
Cefalu Wave <luke.evan.graham@gmail.com>
2019-03-08 11:22:02 -0800

    user input goes into array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,18 +4,37 @@ var chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n
 var randomLetter = chars[Math.floor(Math.random() * chars.length)];
 console.log(randomLetter);
 
-// USER GUESSES WHICH LETTER THE APP CHOSE
+var wins = 0;
+var losses = 0;
+var guessesLeft = 0;
+var guesses = [];
+
+if (guesses.length < 9) {
+    // USER GUESSES WHICH LETTER THE APP CHOSE
 
     // This function runs whenever the user presses a key.
     document.onkeyup = function(event) {
 
         // Determines which key was pressed.
         var userGuess = event.key;
-        console.log(userGuess);
+        guesses.push(userGuess);
+        console.log(guesses);
+    
     }
+} else {
+    console.log("YOU LOSE!")
+}
 
-var wins = 0;
-var losses = 0;
+// for (let i = 0; i < 9; i++) {
+    // const element = array[i];
+    // USER GUESSES WHICH LETTER THE APP CHOSE
 
+    // This function runs whenever the user presses a key.
+    // document.onkeyup = function(event) {
 
-var guessesLeft = 0;
+        // Determines which key was pressed.
+        // var userGuess = event.key;
+        // console.log(userGuess);
+        // i++;
+    // }
+// }
