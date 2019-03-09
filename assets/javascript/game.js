e16debb8845ad959d63b4e4334d2106850ab1149
Cefalu Wave <luke.evan.graham@gmail.com>
2019-03-08 17:15:40 -0800

    exploring printing an array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,13 +6,25 @@ console.log(randomLetter);
 
 var wins = 0;
 var losses = 0;
-var guessesLeft = 0;
+var guessesLeft = 9;
 var guesses = [];
 
-// var targetDiv = document.getElementById("empty-div");
-// targetDiv.textContent = "Hello Friends!";
+// DISPLAY THE WINS COUNTER
+var winsDiv = document.getElementById("wins-div");
+winsDiv.textContent = "Wins: " + wins;
 
-var winsDiv = document.getElementById("wins-div").textContent = "Hello friends!";
+// DISPLAY THE LOSSES COUNTER
+var lossesDiv = document.getElementById("losses-div");
+lossesDiv.textContent = "Losses: " + losses;
+
+// DISPLAY THE GUESSES LEFT COUNTER
+var guessesLeftDiv = document.getElementById("guessesLeft-div");
+guessesLeftDiv.textContent = "Guesses Left: " + guessesLeft;
+
+// DISPLAY THE CURRENT GUESSES
+var guessesDiv = document.getElementById("guesses-div");
+guessesDiv.textContent = "Your Guesses so far: ";
+
 
     // USER GUESSES WHICH LETTER THE APP CHOSE
 
@@ -26,17 +38,27 @@ var winsDiv = document.getElementById("wins-div").textContent = "Hello friends!"
         console.log(guesses);
         console.log(guesses.length);
 
+        // working with something from a website:
+        var z = document.createElement('p');
+        z.innerHTML = 'test satu dua tiga';
+        guessesDiv.appendChild(z);
+
+
+        // guessesDiv.appendChild('guesses');
+
+        // guessesDiv.appendChild(guesses[guesses.length]);
+
             if (guesses[guesses.length - 1] === randomLetter) {
                 console.log("YOU WIN!");
                 wins++;
+                winsDiv.textContent = "Wins: " + wins;
             }
             
         } else {
             console.log("YOU LOSE");
             losses++;
+            lossesDiv.textContent = "Losses: " + losses;
         }
-
-        // Determines which key was pressed.
         
     // }
     
